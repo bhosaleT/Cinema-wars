e2bda87bab96d1c745bccf04a3107a8b1bd34ccd
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-01 03:36:00 +0530

    code formatting

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import queryString from 'query-string';
-import api from '../utils/api';
+import React from "react";
+import queryString from "query-string";
+import api from "../utils/api";
 
 export default class Results extends React.Component {
+  componentDidMount() {
+    var movies = queryString.parse(this.props.location.search);
 
-    componentDidMount () {
-        var movies = queryString.parse(this.props.location.search);
+    api
+      .battle([movies.movieOneName, movies.movieTwoName])
+      .then(function(results) {
+        console.log(results);
+      });
+  }
 
-        api.battle([
-            movies.movieOneName,
-            movies.movieTwoName
-        ]).then(function(results){
-            console.log(results);
-        } )
-    }
-
-    render(){
-        return <div>
-        battle
-        </div>;
-    }
+  render() {
+    return <div>battle</div>;
+  }
 }
