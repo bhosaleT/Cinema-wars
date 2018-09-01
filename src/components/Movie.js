05c5848f7ad879bc7b3c1a75b5e8f30eb073312d
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-09-01 21:11:15 +0530

    added UI for the results page

---
+++ src/components/Movie.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie(props) {
+  return (
+    <div>
+      <h1
+        style={
+          props.label === "winner"
+            ? {
+                background: "#d3d3d3",
+                borderRadius: "10px",
+                boxShadow: "0 10px 20px #888888"
+              }
+            : null
+        }
+        className="movie-header"
+      >
+        {props.label}
+      </h1>
+      <div className="movie-result">
+        <img
+          className="movie-image"
+          src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`}
+          alt={props.movie.title}
+        />
+        <div className="movie-info">
+          <p>
+            {props.movie.vote_average}
+            /10
+          </p>
+        </div>
+        <div className="overlay">
+          <h3 className="overview-heading">Overview</h3>
+          <p className="text">"{props.movie.overview}"</p>
+          <p className="release-date">{props.movie.release_date}</p>
+        </div>
+      </div>
+      <h3 className="movie-name">{props.movie.title}</h3>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+export default Movie;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,19 +1,67 @@
 import React from "react";
 import queryString from "query-string";
 import api from "../utils/api";
+import { Link } from "react-router-dom";
+import Movie from "./Movie";
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    };
+  }
   componentDidMount() {
     var movies = queryString.parse(this.props.location.search);
 
-    api
-      .battle([movies.movieOneName, movies.movieTwoName])
-      .then(function(results) {
-        console.log(results);
+    api.battle([movies.movieOneName, movies.movieTwoName]).then(results => {
+      if (results === null) {
+        return this.setState(() => {
+          return {
+            error: "Looks like there was an error. Check that the movie exists",
+            loading: false
+          };
+        });
+      }
+
+      this.setState(() => {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        };
       });
+    });
   }
 
   render() {
-    return <div>battle</div>;
+    const error = this.state.error;
+    const winner = this.state.winner;
+    const loser = this.state.loser;
+    const loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading</p>;
+    }
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to="/battle">Reset</Link>
+        </div>
+      );
+    }
+
+    return (
+      <div className="form-container">
+        <Movie label="winner" score={winner.vote_average} movie={winner} />
+        <Movie label="loser" score={loser.vote_average} movie={loser} />
+      </div>
+    );
   }
 }
